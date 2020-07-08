69d69e2af5985b755a7106fd599479d40307f702
Rajeev <rajeevsuchiha@gmail.com>
2020-07-08 13:11:28 +0530

    Added: Code to loop multiple times and store singlet and determine percent added

--- flipCoinCombi.sh
+++ flipCoinCombi.sh
@@ -2,11 +2,25 @@ echo "**********Welcome to Flip Coin Combination**********"
 
 #!/bin/bash -x
 
-number=$((RANDOM % 2))
-if [ $number -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+declare -A coin
+read -p "Number of times to flip a coin:" n
+echo "Number of times coin to be flipped: $n"
 
+for (( i=0; i<$n; i++ ))
+do
+	number=$((RANDOM % 2))
+	if [ $number -eq 1 ]
+	then
+		coin['h']=$((${coin[h]} + 1))
+	else
+		coin['t']=$((${coin[t]} + 1))
+	fi
+done
+
+echo "Head count: ${coin[h]}"
+echo "Tail count: ${coin[t]}"
+
+perc=`echo ${coin[h]} $n |  awk '{print ($1/$2)*100}'`
+echo "h ->" $perc "%"
+perc=`echo ${coin[t]} $n |  awk '{print ($1/$2)*100}'`
+echo "t ->" $perc "%"
