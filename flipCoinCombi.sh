7f7db82ddb762ce3058a132e753d551923ea0bb6
Rajeev <rajeevsuchiha@gmail.com>
2020-07-08 13:58:57 +0530

    Added: Code to loop and store triplet and determine percent added

--- flipCoinCombi.sh
+++ flipCoinCombi.sh
@@ -5,7 +5,7 @@ echo "**********Welcome to Flip Coin Combination**********"
 declare -A coin
 read -p "Number of times to flip a coin:" n
 echo "Number of times coin to be flipped: $n"
-echo -e "1.Singlet Combination\n2.Doublet Combination"
+echo -e "1.Singlet Combination\n2.Doublet Combination\n3.Triple Combination"
 read -p "Enter option:" option
 case $option in
 	1)
@@ -51,6 +51,55 @@ case $option in
         echo "TH ->" $perc "%"
         perc=`echo ${coin[ht]} $n | awk '{print ($1/$2)*1oo}'`
         echo "HT ->" $perc "%"
-        perc=`echo ${coin[hh]} $n | awk '{print ($1/$2)*1oo}'`
-        echo "HH ->" $perc "%"
+        perc=`echo ${coin[hh]} $n | awk '{print ($1/$2)*1oo}'`        
+	echo "HH ->" $perc "%"
+	;;
+	3)
+	for((i=0; i<$n; i++))
+        do
+                number=$((RANDOM % 8 + 1))
+                case $number in
+                                1) coin[ttt]=$((${coin[ttt]} + 1))
+                                ;;
+                                2) coin[tth]=$((${coin[tth]} + 1))
+                                ;;
+                                3) coin[tht]=$((${coin[tht]} + 1))
+                                ;;
+                                4) coin[thh]=$((${coin[thh]} + 1))
+                                ;;
+				5) coin[htt]=$((${coin[htt]} + 1))
+                                ;;
+                                6) coin[hth]=$((${coin[hth]} + 1))
+                                ;;
+                                7) coin[hht]=$((${coin[hht]} + 1))
+                                ;;
+                                8) coin[hhh]=$((${coin[hhh]} + 1))
+                                ;;
+                esac
+        done
+	echo "TTT count: ${coin[ttt]}"
+        echo "TTH count: ${coin[tth]}"
+        echo "THT count: ${coin[tht]}"
+        echo "THH count: ${coin[thh]}"
+	echo "HTT count: ${coin[htt]}"
+        echo "HTH count: ${coin[hth]}"
+        echo "HHT count: ${coin[hht]}"
+        echo "HHH count: ${coin[hhh]}"
+	perc=`echo ${coin[ttt]} $n | awk '{print ($1/$2)*1oo}'`
+        echo "TTT ->" $perc "%"
+        perc=`echo ${coin[tth]} $n | awk '{print ($1/$2)*1oo}'`
+        echo "TTH ->" $perc "%"
+        perc=`echo ${coin[tht]} $n | awk '{print ($1/$2)*1oo}'`
+        echo "THT ->" $perc "%"
+        perc=`echo ${coin[thh]} $n | awk '{print ($1/$2)*1oo}'`
+        echo "THH ->" $perc "%"
+	perc=`echo ${coin[htt]} $n | awk '{print ($1/$2)*1oo}'`
+        echo "HTT ->" $perc "%"
+        perc=`echo ${coin[hth]} $n | awk '{print ($1/$2)*1oo}'`
+        echo "HTH ->" $perc "%"
+        perc=`echo ${coin[hht]} $n | awk '{print ($1/$2)*1oo}'`
+        echo "HHT ->" $perc "%"
+        perc=`echo ${coin[hhh]} $n | awk '{print ($1/$2)*1oo}'`
+        echo "HHH ->" $perc "%"
+	;;
 esac
